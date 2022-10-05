ffc18bc60bf9ea5205f2402b5ccbb0f36d9ae526
jair-felix <jair_alarcon@usmp.pe>
2022-10-04 20:38:10 -0500

    spotify

---
+++ src/main/java/com/example/demo/integration/spotify/dto/Followers.java
@@ -0,0 +1,14 @@
+package com.example.demo.integration.spotify.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Followers{
+
+    private int total; 
+
+}
